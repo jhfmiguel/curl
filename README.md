e59dd8276ca2e4b15e6e3733200a18a34fbd7222
jhfmiguel <jhfmiguel@hotmail.com>
2019-02-20 22:07:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cURL PHP
+
+This repository contains a simple code of cURL, PHP and Via Cep Api.
---
+++ curlCep.php
@@ -0,0 +1,18 @@
+<?php
+
+$cep = "75123470";
+$link = "https://viacep.com.br/ws/$cep/json/";
+
+$ch = curl_init($link);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+print_r($data);
+//print_r($data["localidade"]);
